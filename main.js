9e01418bc94442ec94c6d26944464d7a803c7092
view-website <91933827+view-website@users.noreply.github.com>
2024-08-10 12:14:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+function identify(){
+  if (document.getElementById("plant-name").value === "holy basil"){
+    document.getElementById("reccommendations").innerHTML = "Nitrogen, Potassium, Phosphorous in a 10:10:10 ratio"
+  }
+}
