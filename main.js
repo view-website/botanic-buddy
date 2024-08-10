c980d65814b6bb462d5444c08636815d52c7b313
view-website <91933827+view-website@users.noreply.github.com>
2024-08-10 13:22:46 +0530

    Update main.js 2

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-
-function identify(){
-  if (document.getElementById("plant-name").value === "holy basil"){
-    document.getElementById("reccommendations").innerHTML = "Nitrogen, Potassium, Phosphorous in a 10:10:10 ratio"
+function identifypl() {
+  var plant = document.getElementById("plant-name").value;
+  if (plant == "holy basil"){
+    document.getElementById("reccommendations").style.color = "white";
+    document.getElementById("reccommendations").innerHTML = "Potassium, Nitrogen, Phosphorous in 10:10:10 ratio";
+    console.log("Nitrogen, Potassium, Phosphorous in a 10:10:10 ratio");
+    document.getElementById("schedule").innerHTML = "1 inch of water per week";
   }
 }
